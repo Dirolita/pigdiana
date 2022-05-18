22da30ee531bc1b1162706b052ce760756fec412
DianaCaro <dianacaroserranot@gmail.com>
2022-05-18 08:31:16 -0500

    prueba

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          prueba
+          mi prueba
         </p>
         <a
           className="App-link"
