2187a3bd703080e955d00eec46036c848b49f6bc
DianaCaro <dianacaroserranot@gmail.com>
2022-05-18 10:42:45 -0500

    created modules modified menu´styles

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/header/header';
+import Navbar from './components/navbar/navbar';
+import Footer from './components/footer/footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header/>
+    <Footer/>
   </React.StrictMode>
 );
 
