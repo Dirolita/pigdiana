56bd2959e6e54f033d050085858096ab2df085df
DianaCaro <dianacaroserranot@gmail.com>
2022-05-25 11:58:28 -0500

    trabaje en la pagina home

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import Header from "../components/header/header";
+import Chome from "../components/Comphome/Chome";
+import Footer from "../components/footer/footer";
+function Home(){
+    return(
+        <div>
+            <Header/>
+            <Chome />
+            <Footer />
+        </div>
+    )
+}
+export default Home;
