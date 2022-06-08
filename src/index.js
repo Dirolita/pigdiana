aed8ec08831816ec2837b777f8521290789c6cef
DianaCaro <dianacaroserranot@gmail.com>
2022-06-08 16:34:52 -0500

    cambio direccion

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <Routes>
-            <Route path='/dianapigm/' element={<Home />}></Route>
+            <Route path='/pigdiana/' element={<Home />}></Route>
             <Route path='/' element={<Home />}></Route>
             <Route path='/home' element={<Home />}></Route>
             <Route path='/talleres' element={<Talleres />}></Route>
